f27532261dcb565bbe810a1ea8c8fa725460b201
NandhiniKrishnamoorthy <nandhiniprasath25@gmail.com>
2021-09-28 23:26:51 -0700

    Docs: Initial commit
    completed this project in local server

---
+++ index.js
@@ -0,0 +1,35 @@
+﻿// JavaScript source code
+var dob = document.querySelector("#date-of-birth");
+var luckyNumber = document.querySelector("#lucky-number");
+var checkButton = document.querySelector("#check-birthdate");
+var displayMessage = document.querySelector("#output-message");
+
+function calculateSum(num) {
+    var sum = 0;
+    for (i = 0; i < num.length; i++) {  
+        sum = sum + Number(num.charAt(i));        
+    }
+    return sum;
+}
+
+function showMessage(msg) {
+    displayMessage.innerText = msg;
+}
+
+checkButton.addEventListener("click", function checkBirthDateLucky() {    
+    var dateOfBirth = dob.value;
+    dateOfBirth = dateOfBirth.replaceAll("-", "");
+    var dob_sum = calculateSum(dateOfBirth);
+    var luckynum = luckyNumber.value;
+    if (dateOfBirth && luckynum) {
+        if (dob_sum % luckynum === 0) {
+            showMessage("Wohoo! Your Birthday Date is Lucky! 🥳");
+        }
+        else {
+            showMessage("Oops! Your BirthDate is not Lucky! 😕");
+        }
+    }
+    else {
+        showMessage("Please enter both the fields");
+    }
+});
